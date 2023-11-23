ef0e6a3a9b54e8a117b26c2efe4332ccf20ed731
puiuioanadiana <137444855+puiuioanadiana@users.noreply.github.com>
2023-11-23 19:45:34 +0000

    first version

--- text-app/src/App.js
+++ text-app/src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import React from 'react';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-     <p>Ioana</p>
+    <main>
+    <Header />
+    <div className="content">
+     <p>This is the content</p>
     </div>
+    <Footer />
+    </main>
   );
 }
 
